30dad852c500e6e67f48ee7282960716c83a81bc
Miryam <mbathilde@gmail.com>
2021-09-30 11:34:04 +0200

    Carácteres especiales en JAVA

--- Variables/src/ConcatenaciónDeCadenas.java
+++ Variables/src/ConcatenaciónDeCadenas.java
@@ -11,8 +11,14 @@ public class ConcatenaciónDeCadenas {
         var i = 3;
         var j = 4;
         
+        // evaluación siempre de izquierda a derecha
         System.out.println(i + j); // 7
+        //evaluación de izq a der, realiza suma
         System.out.println(i + j + usuario);// 7Miryam
+        // contexto cadena, todo es una cadena
         System.out.println(usuario + i + j); // Miryam34 - Aqui no suma los num
+        // uso de parentesis modifica la prioridad en la evaluacion
+        // aqui si que lo suma, porque los parentesis toma la máxima prioridad
+        System.out.println(usuario + (i + j));//Miryam7
     }
 }
