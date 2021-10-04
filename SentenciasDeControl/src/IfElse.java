31bd0b3184d4f9857b8b9f8f5f41f703a7e21850
Miryam <mbathilde@gmail.com>
2021-10-04 12:02:57 +0200

    Estaciones del año con if else

--- SentenciasDeControl/src/IfElse.java
+++ SentenciasDeControl/src/IfElse.java
@@ -1,39 +1,21 @@
 
 public class IfElse {
-    public static void main (String args[]){
-        
-        var condicion = true;
-        
-        if (condicion){
-            System.out.println("Condición verdadera");
+    public static void main(String args[]) {
+
+        var mes = 1;
+        var estacion = "Estacion desconocida";
+
+        if (mes == 1 || mes == 2 || mes == 12) {
+            estacion = "Invierno";
+        } else if (mes == 3 || mes == 4 || mes == 5) {
+            estacion = "Primavera";
+        } else if (mes == 6 || mes == 7 || mes == 8) {
+            estacion = "Verano";
+        } else if (mes == 9 || mes == 10 || mes == 11) {
+            estacion = "Otoño";
         }
-        else {
-            System.out.println("Condición falsa");
-        }
         
-        // vamos a convertir un numero en letra
-        
-        var numero = 2;
-        var numeroTexto = "Numero desconocido";
-        
-        // con el else if evitamos que el programa se ejecute en cada condicion a evaluar
-        // ya que en cuanto una condición se cumpla para su ejecución y no sigue
-        if(numero == 1){
-            numeroTexto = "Numero 1";
-        }
-        else if (numero == 2){
-            numeroTexto = "Numero 2";
-        }
-        else if (numero == 3){
-            numeroTexto = "Numero 3";
-        }
-        else if (numero == 4){
-            numeroTexto = "Numero 4";
-        }
-//        else {
-//            numeroTexto = "Numero no encontrado";
-//        }
-        
-        System.out.println("numeroTexto = " + numeroTexto);
+        System.out.println("estacion = " + estacion);
+
     }
 }
